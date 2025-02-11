7f326859c21e236ff19776b1a5d043f3916910d6
Hammerflame <commeyne.reinout@gmail.com>
2025-02-11 16:45:28 +0100

    kopieer know oef6

--- Semester II/Labo_2/oefening_6/scripts/code.js
+++ Semester II/Labo_2/oefening_6/scripts/code.js
@@ -11,13 +11,14 @@ const setup = () => {
         };
     })();
 
-    let btnKopieer = document.getElementById("btnKopieer");
-    btnKopieer.addEventListener("click", kopieer);
-
     const kopieer = () => {
         let txtInput = document.getElementById("txtInput");
         let tekst = txtInput.value;
         console.log(tekst);
     }
+
+    let btnKopieer = document.getElementById("btnKopieer");
+    btnKopieer.addEventListener("click", kopieer);
 }
+
 window.addEventListener("load", setup);
